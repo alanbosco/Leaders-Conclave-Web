5a9771a6aa1763f6a54b184e10a4370df3c3efcb
Ijaz <mohammedijas619@gmail.com>
2017-10-13 14:37:08 +0530

    Content edited

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+
+$field_name = $_POST["name"];
+$field_email = $_POST["mail"];
+$field_sub = $_POST["sub"];
+$field_message = $_POST["message"];
+
+$to = "info@leadersconclave.in";
+$subject = "MAIL FROM SITE a VISITOR".$field_name;
+$message = "NAME:".$field_name."\n EMAIL-ID:".$field_email."\n SUBJECT:".$field_sub."\n MESSAGE".$field_message;
+
+if(mail($to,$subject,$message))
+ {
+      echo "<script>alert('Your Message was sent Successfully. Thank You For Your Time !');</script>";
+      }
+      else
+      {
+      echo "<script>alert('Something wrong happened. Please try again later. Sorry For The Trouble');		            </script>";
+      }
+    
+
+?>
+<meta http-equiv="refresh" content="2; url=contact.html">
+<!--<meta http-equiv="refresh" content="2; url=../image_slider/ads.html">
+header("Location: ../image_slider/ads.html");-->
